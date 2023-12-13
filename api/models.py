c8b9a66a9f4cf06da32362d18ae1ca0de2fee5ac
ashwabu <abdulazizredi90@gmail.com>
2023-12-13 19:32:22 +0300

    seting app rest api

--- api/models.py
+++ api/models.py
@@ -4,6 +4,7 @@ class Todo(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=200)
     is_done = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
